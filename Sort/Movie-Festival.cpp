1674fe13f3a70229bcc11e82cd299d85262d9611
zhichao zhang <jnmaopopo.zhang@gmail.com>
2022-01-26 21:57:00 -0500

    add movie festival

---
+++ Sort/Movie-Festival.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//----------TEMPLATES----------
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {cerr << *it << "=" << a << ", "; err(++it, args...);}
+
+// -----------TYPE DEFINE------
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+typedef vector<pi> vpi;
+//----------MACROS----------
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+
+#define seea(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define sees(s,n) for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define all(x) x.begin(),x.end()
+#define rep(i,a,b) for (int i=a; i<b; ++i)
+#define rev(i,a,b) for (int i=a; i>b; --i)
+#define trav(a, x) for (auto& a : x)
+
+
+const int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const int MX = 200005;
+const ld PI = 4*atan((ld)1);
+
+int main() {
+    int n ;
+    cin >> n;
+    int a, b;
+    vpi arr;
+    rep(i, 0, n) {
+        cin >> a; cin >> b;
+        arr.pb({a,b});
+    }
+    sort(all(arr), [](pi A, pi B){
+        return A.s == B.s ? A.f < B.f : A.s < B.s;
+    });
+    int res= 0;
+    int cur = 0;
+    rep(i, 0, n) {
+        if(arr[i].f >= cur) {
+            cur = arr[i].s;
+            res +=1;
+        }
+    }
+    cout << res;
+}
