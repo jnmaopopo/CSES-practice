415d3083d5a7d437c235e4540abd805fcb8d812f
sophieLI0308 <91570542+sophieLI0308@users.noreply.github.com>
2022-02-05 18:34:38 -0500

    add 2 sorting questions

---
+++ Sort/Playlist.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//----------TEMPLATES----------
+template <typename... T>
+void see(T &...args) { ((cin >> args), ...); }
+template <typename... T>
+void put(T &&...args) { ((cout << args << " "), ...); }
+template <typename... T>
+void putl(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << '\n';
+}
+#define error(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << "=" << a << ", ";
+    err(++it, args...);
+}
+
+// -----------TYPE DEFINE------
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<pi> vpi;
+//----------MACROS----------
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+
+#define seea(a, x, y)           \
+    for (int i = x; i < y; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define seev(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.push_back(x);         \
+    }
+#define sees(s, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        s.insert(x);            \
+    }
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rev(i, a, b) for (int i = a; i > b; --i)
+#define trav(a, x) for (auto &a : x)
+
+const int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const int MX = 200005;
+const ld PI = 4 * atan((ld)1);
+
+namespace io
+{
+    void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+    void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+    void setIO(string s = "")
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); // fast I/O
+        if (sz(s))
+        {
+            setIn(s + ".in"), setOut(s + ".out");
+        } // for USACO
+    }
+}
+
+using namespace io;
+int n, x;
+int main()
+{
+    setIO();
+    cin >> n;
+    int l = 0, r = 0;
+    int a[n];
+    seea(a, 0, n);
+    set<int> c;
+    int res = 0;
+    while (r < n)
+    {
+        while (r < n && !c.count(a[r]))
+        {
+            c.insert(a[r]);
+            r++;
+        }
+        res = max(res, r - l);
+        c.erase(a[l]);
+        ++l;
+    }
+    cout << res;
+}
